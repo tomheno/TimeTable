f9ae7af85da28d89d3cb392b9839df71e3418ffd
own <tom@heneau.lt>
2017-09-23 21:48:58 +0200

    Some tweaks

--- datas.php
+++ datas.php
@@ -19,7 +19,11 @@
     /* Tab used for coloration */
     $used_id = array(); // Contains the pair [color, title]
     // You can add many colors
-    $colors = array("#e8d174","#e39e54","#d64d4d","#4d7358","#9ed670","#6c2a7d","#9ed8f5","#0093d0","#ef4135","#99d5cf","#009688","#ffdbac","#ff99cc","#ff3e96","#6959cd","#4682B4","#00BFFF","#00113a","#f7f7f7","#313131","#31ef78","#3df249","#d6a2fd","#eabcff","#8cceff","#0092ff","#bca7d2","#e1e2df","#c3c6c0","#d2d6d7","#eaeeef","#ee4400","#333377","#336644","#335566","#b4155d","#d3ffce","#ffaddf","#9be466","#92e9ff","#99d9fa","#d3ffce","#39697e","#008000","#0000ff","#fff7dd","#f0cebb","#f2c8b2","#66ffff","#4df1ff","#00ebff","#ffe4e1","#d3ffce","#9be466","#999999","#e5b451","#a9f114",);
+    $colors = array("#588C73","#F2E394","#F2AE72","#D96459","#8C4646",);
+    $colors = array_merge($colors,$colors);
+    $colors = array_merge($colors,$colors);
+    $colors = array_merge($colors,$colors);
+    $colors = array_merge($colors,$colors);
     $current = -1;
     while(!feof($file)){
         $line = fgets($file); // separate the ""
@@ -62,13 +66,13 @@
             default:
                 // Nothing special to do here :)
                 break;
-        }	
-		}	
+        }
+		}
 
     }
     fclose($file);
     header("Content-type: text/json");
     echo json_encode($edt);
     die();
-	
+
 	?>
